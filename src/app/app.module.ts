273fcfebe35a92121d397f2c09b6603fa84d5456
Andrey Shklyaruk <13mailg@gmail.com>
2018-05-29 12:02:04 +0300

    EventsListComponent created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
+import { EventsListComponent } from './events/events-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
+  imports: [BrowserModule],
   declarations: [
-    EventsAppComponent
-  ],
-  imports: [
-    BrowserModule
+    EventsAppComponent,
+    EventsListComponent
   ],
   providers: [],
   bootstrap: [EventsAppComponent]

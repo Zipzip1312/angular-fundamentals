5e278138b26e395a764836e8f09a6e5159863902
Andrey Shklyaruk <13mailg@gmail.com>
2018-06-08 15:42:04 +0300

    Close Modal method created for simple modal component

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Inject, ViewChild, ElementRef } from '@angular/core';
+import { JQ_TOKEN } from "./jQuery.service";
 
 @Component({
   selector: 'simple-modal',
   template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             <h4 class="modal-title">{{title}}</h4>
           </div>
-          <div class="modal-body">
+          <div class="modal-body" (click)="closeModal()">
             <ng-content></ng-content>
           </div>
         </div>
@@ -24,11 +25,14 @@ import { Component, OnInit, Input } from '@angular/core';
   `]
 })
 
-export class SimpleModalComponent implements OnInit {
+export class SimpleModalComponent {
   @Input() title: string
   @Input() elementId: string
+  @ViewChild('modalcontainer') containerEl: ElementRef
 
-  constructor() { }
+  constructor( @Inject(JQ_TOKEN) private $: any ) { }
 
-  ngOnInit() { }
+  closeModal(){
+    this.$(this.containerEl.nativeElement).modal('hide');
+  }
 }

809813a71e63eea924b54dcee4c927c58cf9de4d
Andrey Shklyaruk <13mailg@gmail.com>
2018-06-12 14:31:04 +0300

    Navbar events list created

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,5 +1,5 @@
 import { EventService } from './../events/shared/event.service';
-import { ISession } from './../events/shared/event.model';
+import { ISession, IEvent } from './../events/shared/event.model';
 import { AuthService } from './../user/auth.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -14,14 +14,21 @@ import { Component, OnInit } from '@angular/core';
   `]
 })
 
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   serachTerm: string = ""
   foundSessions: ISession[]
+  eventsList: IEvent[]
 
   constructor (
     public auth:AuthService,
     private eventService: EventService
   ){}
+
+  ngOnInit(){
+    this.eventService.getEvents().subscribe(data => {
+      this.eventsList = data
+    })
+  }
 
   searchSessions(serachTerm){
     this.eventService.searchSessions(serachTerm).subscribe(
